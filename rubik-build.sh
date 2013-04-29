70b0be5796d8461e459cb77c4e137f3a6d6e276e
BigBrother1984 <stevewatersy@gmail.com>
2013-04-29 17:49:12 +0200

    Kernel: RUBIK r30

--- rubik-build.sh
+++ rubik-build.sh
@@ -46,7 +46,7 @@ else
   #
   # LOCAL KERNEL VERSION
   #
-  ak_ver="RUBIK-AK-r29"; export LOCALVERSION="~"`echo $ak_ver`
+  ak_ver="RUBIK-AK-r30"; export LOCALVERSION="~"`echo $ak_ver`
 
   debug=0
 
