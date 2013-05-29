5f1ea3fae7e969a447f1e1ba9c274c8262828ab8
BigBrother1984 <stevewatersy@gmail.com>
2013-05-29 23:56:05 +0200

    opp4xxx_data.c: disable 1480 Mhz freq. Max at 1420.

--- arch/arm/mach-omap2/opp4xxx_data.c
+++ arch/arm/mach-omap2/opp4xxx_data.c
@@ -509,7 +509,7 @@ int __init omap4_opp_init(void)
 			omap4_mpu_opp_enable(1350000000);
 #ifdef CONFIG_OMAP_OCFREQ_1400
 			omap4_mpu_opp_enable(1420000000);
-			omap4_mpu_opp_enable(1480000000);
+			// omap4_mpu_opp_enable(1480000000);
 #endif
 #ifdef CONFIG_OMAP_OCFREQ_1600
 			omap4_mpu_opp_enable(1560000000);
